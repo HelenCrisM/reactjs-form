04748564d8a9622edf18466250767afe546138fe
Helen Cris <helencristavares@gmail.com>
2024-02-26 15:12:47 -0300

    ✨ feat: created Home Page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Form from '../../components/Form';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return <Form />;
+}
+
+export default Home;
