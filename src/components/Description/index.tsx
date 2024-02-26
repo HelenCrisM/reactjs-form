1074c4fbf5738acde50dd5f7e68f3d1833dbd493
Helen Cris <helencristavares@gmail.com>
2024-02-25 23:22:04 -0300

    ✨ feat: created Description component

---
+++ src/components/Description/index.tsx
@@ -0,0 +1,7 @@
+import Styled from './styles';
+
+const Description = ({ children }: any) => {
+	return <Styled.Description>{children}</Styled.Description>;
+};
+
+export default Description;
---
+++ src/components/Description/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import theme from '../../theme';
+
+const Description = styled.p`
+	color: ${theme.colors.gray};
+`;
+
+export default { Description };
